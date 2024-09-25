d61f9a9bcfd774a01a137c697c73c8098406ade5
matthewrib <matthewrib1028@gmail.com>
2024-09-24 18:25:58 -0700

    finished the functionallity

--- VendingMachine/VendingMachine.cpp
+++ VendingMachine/VendingMachine.cpp
@@ -6,56 +6,69 @@
 */
 
 #include <iostream>
+#include <iomanip>
+#include <string>
 
 using namespace std;
 
-void purchaseItem(double cost);
+void purchaseItem(double cost, string item);
 
 int main() {
-	const double chips = 1.50;
-	const double snickers = 1.75;
-	const double cookies = 2.00;
-	int select;
+    const double chips = 1.50;
+    const double snickers = 1.75;
+    const double cookies = 2.00;
+    int select;
+    bool buying = false;
 
-	cout << "Welcome to the Cypress College Vending Machine! \n";
-	cout << "Item 1: Bag of Chips = " << chips << "\n Item 2: Snickers Bar = " << snickers << "\n Item 3: Cookies = " << cookies << "\n";
-	cout << "Enter the number of the item you wish to purchase: ";
-	cin >> select; 
-	
-	switch (select) {
-		case 1: 
-			purchaseItem(chips);
-			break;
-		case 2:
-			purchaseItem(snickers);
-			break;
-		case 3: 
-			purchaseItem(cookies);
-			break;
-		default:
-			cout << "Invalid selection. ";
-			cout << "Enter the number of the item you wish to purchase: ";
-			cin >> select;
-			break;
-	}
+    cout << fixed << setprecision(2);
+    cout << "Welcome to the Cypress College Vending Machine! \n";
+    cout << "Item 1: Bag of Chips = $" << chips << "\n";
+    cout << "Item 2: Snickers Bar = $" << snickers << "\n";
+    cout << "Item 3: Cookies = $" << cookies << "\n";
+    cout << "Enter the number of the item you wish to purchase: ";
+    cin >> select;
 
-	return 0;
+    while (buying == false) {
+        switch (select) {
+        case 1:
+            purchaseItem(chips, "bag of chips");
+            buying = true;
+            break;
+        case 2:
+            purchaseItem(snickers, "Snickers");
+            buying = true;
+            break;
+        case 3:
+            purchaseItem(cookies, "cookies");
+            buying = true; 
+            break;
+        default:
+            cout << "Invalid selection. \n";
+            cout << "Enter the number of the item you wish to purchase: ";
+            cin >> select;
+            break;
+        }
+    }
+
+    return 0;
 }
 
-void purchaseItem(double cost) {
-	double payment = 0;
-	double enter, change;
-	while (cost >= payment) {
-		cout << "Enter coin amount to put in machine (0.01, 0.05, 0.10, 0.25, or 1.00): ";
-		cin >> enter;
-		if (enter != 0.01 && enter != 0.05 && enter != 0.10 && enter != 0.25 && enter != 1.00) {
-			cout << "You entered an invalid amount. \n";
-		}
-		else {
-			cout << "Total amount entered: " << enter << ".\n";
-		}
-	}
-	change = payment - cost;
-	cout << "Enjoy! \n";
-	cout << "Your change is " << change << ".";
+void purchaseItem(double cost, string item) {
+    double payment = 0;
+    double enter, change;
+    cout << fixed << setprecision(2);
+    while (cost > payment) {
+        cout << "Enter coin amount to put in machine (0.01, 0.05, 0.10, 0.25, or 1.00): ";
+        cin >> enter;
+        if (enter != 0.01 && enter != 0.05 && enter != 0.10 && enter != 0.25 && enter != 1.00) {
+            cout << "You entered an invalid amount. \n";
+        }
+        else {
+            payment += enter;
+            cout << "Total amount entered: $" << payment << ".\n";
+        }
+    }
+    change = payment - cost;
+    cout << "Enjoy your " << item << "!\n";
+    cout << "Your change is $" << change << ".\n";
 }
